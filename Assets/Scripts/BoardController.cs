06daa808767bcaa5e8345496a0459099bae3ad38
Daniel de Oliveira <lacrimaopera@gmail.com>
2016-10-07 21:31:33 -0300

    Pequenos de ajustes

--- Assets/Scripts/BoardController.cs
+++ Assets/Scripts/BoardController.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class BoardController : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start() 
-    {
-        // Contabilizar todas as casas do tabuleiro
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            transform.GetChild(i).name = "casa" + i;
-        }
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		// Contabilizar todas as casas do tabuleiro
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild (i).name = "casa" + i;
+		}
+	}
 
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,89 +4,85 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private int valorDado, posAtual, proxPos, tamTabuleiro;
-    private bool emMovimento;
-    private Transform proxPosTransform;
+	private int valorDado, posAtual, proxPos, tamTabuleiro;
+	private bool emMovimento;
+	private Transform proxPosTransform;
 	public Text textCasas;
 
-    // Use this for initialization
-    void Start()
-    {
-        // Setar o tamanho do tabuleiro, 
-        // isso permite termos tabuleiros de qualquer tamanho,
-        // bastando que cada sprite esteja dentro do GameObject "Board" na Unity.
-        tamTabuleiro = GameObject.Find("Board").transform.childCount;
+	// Use this for initialization
+	void Start ()
+	{
+		// Setar o tamanho do tabuleiro, 
+		// isso permite termos tabuleiros de qualquer tamanho,
+		// bastando que cada sprite esteja dentro do GameObject "Board" na Unity.
+		tamTabuleiro = GameObject.Find ("Board").transform.childCount;
 
-        // Setar a próxima posição no início do jogo
+		// Setar a próxima posição no início do jogo
 		proxPos = posAtual + 1;
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update ()
+	{
 		// Desativar a contagem
-		ActivateDiceUI(emMovimento);
+		ActivateDiceUI (emMovimento);
 
-        if (Input.GetKeyDown(KeyCode.D) && !emMovimento)
-        {
-            // "Jogar" o dado
-            valorDado = Random.Range(1, 7);
-            // Ligar o movimento
-            emMovimento = true;
-            // Buscar a nova posição para movimentar o personagem
-			proxPosTransform = GameObject.Find("casa" + proxPos).transform;
+		if (Input.GetKeyDown (KeyCode.D) && !emMovimento) {
+			// "Jogar" o dado
+			valorDado = Random.Range (1, 7);
+			// Ligar o movimento
+			emMovimento = true;
+			// Buscar a nova posição para movimentar o personagem
+			proxPosTransform = GameObject.Find ("casa" + proxPos).transform;
 
-            Debug.Log("Mover " + valorDado + " casas...");
-        }
+			Debug.Log ("Mover " + valorDado + " casas...");
+		}
 
-        if (emMovimento)
-        {
-            // Definição do sentido do movimento
-            // Verificar as casas visitadas
-			if (transform.position != proxPosTransform.position)
-            {
-                // Executar o movimento do personagem
-				transform.position = Vector2.MoveTowards(transform.position, proxPosTransform.position, Time.deltaTime);
-            }
-            else
-            {
-                // Diminuir o valor do dado
-                valorDado--;
-                // Ajustar a posição atual
+		if (emMovimento) {
+			// Definição do sentido do movimento
+			// Verificar as casas visitadas
+			if (transform.position != proxPosTransform.position) {
+				// Executar o movimento do personagem
+				transform.position = Vector2.MoveTowards (transform.position, proxPosTransform.position, Time.deltaTime);
+			} else {
+				// Diminuir o valor do dado
+				valorDado--;
+				// Ajustar a posição atual
 				posAtual = proxPos;
-                // Setar a próxima posição, cuidando o tamanho do tabuleiro
+				// Setar a próxima posição, cuidando o tamanho do tabuleiro
 				proxPos = posAtual == tamTabuleiro - 1 ? 0 : proxPos + 1;
-                // Buscar a próxima posição para movimentar o personagem
-				proxPosTransform = GameObject.Find("casa" + proxPos).transform;
+				// Buscar a próxima posição para movimentar o personagem
+				proxPosTransform = GameObject.Find ("casa" + proxPos).transform;
 
-                // Verificar a próxima jogada do dado,
-                // caso verdadeiro, parar a movimentação.
-                if (valorDado == 0)
-                {
-                    emMovimento = false;
+				// Verificar a próxima jogada do dado,
+				// caso verdadeiro, parar a movimentação.
+				if (valorDado == 0) {
+					emMovimento = false;
 
-                    // Após o personagem terminar a sua movimentação,
-                    // executar alguma outra ação a partir daqui!!! 
-                    Debug.Log("Cheguei ao destino!");
+					// Após o personagem terminar a sua movimentação,
+					// executar alguma outra ação a partir daqui!!! 
+					Debug.Log ("Cheguei ao destino!");
 
-                }
-            }
+				}
+			}
 
 			// Escrever a movimentaçao na tela
-			EscreverCasasAMovimentar(valorDado);
-        }
-    }
+			EscreverCasasAMovimentar (valorDado);
+		}
+	}
 
 	// Escrever na HUD
-	void EscreverCasasAMovimentar(int valor) {
+	void EscreverCasasAMovimentar (int valor)
+	{
 		// Se o campo de texto nao for nulo, escrever
 		if (textCasas != null) {
-			textCasas.text = valor.ToString();
+			textCasas.text = valor.ToString ();
 		}
 	}
 
 	// Ativar o HUD representando os dados
-	void ActivateDiceUI(bool activate) {
-		textCasas.transform.parent.gameObject.SetActive(activate);
+	void ActivateDiceUI (bool activate)
+	{
+		textCasas.transform.parent.gameObject.SetActive (activate);
 	}
 }
